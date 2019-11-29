e89d77517e111340ff1945e7e2f1af69a06970fe
Vincent Zhu <zhu.zhiwei.vincent@gmail.com>
2019-11-29 15:12:43 +0100

    Update script.js
    update fonction getAPIData pour avoir les information du personnage dans input

--- js/script.js
+++ js/script.js
@@ -6,8 +6,24 @@ function getAPIData() {
             const data = JSON.parse(xhr.responseText);
             const lenght=data.results.length
 
-            for (let i = 0;i<=lenght;i++){
+            for (let i = 0;i<lenght;i++){
+                let input = document.getElementById('name').value
                 const result = data.results[i]
+                console.log(result)
+                if(result.name == input){
+
+                    let template =document.querySelector('.info')
+                    template.innerHTML ='<ul><li>name: '+result.name+'</li>'+
+                        '<li>gender : '+result.gender+'</li>'+
+                        '<li>year of birth :'+result.birth_year+'</li>'+
+                        '<li>height : '+result.height+'cm</li>'+
+                        '<li>weight : '+result.mass+'kg</li>'+
+                        '<li>hair color : '+result.hair_color+'</li>'+
+                        '<li>eye color : '+result.skin_color+'</li>'+
+                        '<li>skin color : '+result.skin_color+'</li>'
+
+                }
+
             }
 
 
@@ -16,11 +32,5 @@ function getAPIData() {
     xhr.open('GET', 'https://swapi.co/api/people/   ');
     xhr.send()
 }
-function getperso(){
-    let input = document.getElementById('name').value
-
-}
 
 
-
-getAPIData()
